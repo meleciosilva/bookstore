cb664da266cd406f0a45dff840f98766145c216d
meleciosilva <mpsilva94@gmail.com>
2021-05-29 08:56:38 -0400

    add auth route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 
+// ROUTERS
+const authRouter = require("./routes/auth.router");
 const booksRouter = require("./routes/books.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+// SEEDERS
+const { seedAdmin } = require("./seeders/admins");
+// console.log(seedAdmin())
+
 app.use(express.json());
 
+app.use("/auth", authRouter);
 app.use("/books", booksRouter);
+
 // Not Found
 app.use(notFound);
 // Error Handler
