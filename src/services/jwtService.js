998304950d7b80fecb898e41bfa61f086486d821
meleciosilva <mpsilva94@gmail.com>
2021-05-29 09:39:36 -0400

    add createToken and decodeToken

---
+++ src/services/jwtService.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+const expiry = Number(process.env.TOKEN_EXPIRY);
+
+exports.createToken = (user) => {
+  try {
+    let token = jwt.sign({
+      id: user.user_id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName
+    }, secret, { expiresIn: expiry });
+    return token;
+  } catch(err) {
+      console.log(err);
+      return null;
+  }
+}
+
+exports.decodeToken = (token) => {
+  try {
+    let decodedToken = jwt.verify(token, secret);
+    return decodedToken;
+  } catch(err) {
+      console.log(err);
+      return null;
+}
+}
