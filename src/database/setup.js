9354f2db8a09c526b723693229c2518083f0cc46
meleciosilva <mpsilva94@gmail.com>
2021-05-29 08:56:01 -0400

    add useCreateIndex

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 // SET UP MONGOOSE
 const mongoose = require("mongoose");
 const connectionString = "mongodb://localhost:27017/bookstore";
-
 
 // connect application to database
 module.exports = () => {
   mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
   }, (err) => {
     if (err) return console.log(err);
     console.log("Database Connection is Successful!");
